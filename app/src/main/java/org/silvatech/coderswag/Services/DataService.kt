4c70749320e6e8fc449f01c54133b20388c02bad
csilva <csilva@silvatech.org>
2019-02-09 08:48:51 -0600

    Created product list item resource in order to set up each block with it's display content. String changes were made to remove the zero infront of the pcitures . Also a function to get products using a when statment was added.

--- app/src/main/java/org/silvatech/coderswag/Services/DataService.kt
+++ app/src/main/java/org/silvatech/coderswag/Services/DataService.kt
@@ -15,26 +15,46 @@ object DataService {
 
 
     val hats = listOf(
-        Product("ST Graphic Beanie", "$18","hat01"),
-        Product("ST Hat Black", "$28","hat02"),
-        Product("ST Hat White", "$18","hat03"),
-        Product("ST Hat Snapback", "$22","hat04")
+        Product("ST Graphic Beanie", "$18","hat1"),
+        Product("ST Hat Black", "$28","hat2"),
+        Product("ST Hat White", "$18","hat3"),
+        Product("ST Hat Snapback", "$22","hat4")
     )
 
     val hoodies = listOf(
-        Product("ST Hoodie Gray","$38","hoodie01"),
-        Product("ST Hoodie Red","$50","hoodie02"),
-        Product("ST Hoodie Greyer","$48","hoodie03"),
-        Product("ST Hoodie Black","$60","hoodie04")
+        Product("ST Hoodie Gray","$38","hoodie1"),
+        Product("ST Hoodie Red","$50","hoodie2"),
+        Product("ST Hoodie Greyer","$48","hoodie3"),
+        Product("ST Hoodie Black","$60","hoodie4")
 
     )
 
     val shirts = listOf(
-        Product("ST Shirt Black","$10","shirt01"),
-        Product("ST Shirt Light Grey","$12","shirt02"),
-        Product("ST Hoodie Shirt Red","$22","shirt03"),
-        Product("ST Shirt Hustle E Black","$60","shirt04"),
-        Product("DSLP Kickflip Studios","$18","shirt05")
+        Product("ST Shirt Black","$10","shirt1"),
+        Product("ST Shirt Light Grey","$12","shirt2"),
+        Product("ST Hoodie Shirt Red","$22","shirt3"),
+        Product("ST Shirt Hustle E Black","$60","shirt4"),
+        Product("DSLP Kickflip Studios","$18","shirt5")
 
     )
+
+    val digitalGood = listOf<Product>()
+
+    fun getProducts(category: String): List<Product> {
+
+        return when(category){
+            "SHIRTS" -> return shirts
+            "HATS" -> return hats
+            "HOODIES" -> return hoodies
+            else -> return digitalGood
+
+        }
+
+//        if(category == "HATS") {
+//            return hats
+//        }else if (category == "SHIRTS"){
+//            return shirts
+//        }
+    }
+
 }
